72145d5397f9e7e8bac0bac50c2fdc6b6131b8b9
baselallam <baseljahen@gmail.com>
2025-01-24 13:20:14 +0200

    initCommit

---
+++ lib/notes.dart
@@ -0,0 +1,51 @@
+/*
+
+  - Firebase Integration & Notification
+  - Google Maps Integration
+  - Multi Lang
+  - Deploy
+
+
+
+  - Screens
+    User Module
+      - Login
+      - Register
+      - Reset Password
+      - User Profile
+      - Update User Profile
+      - Forget Password
+      - Enter & Verify OTP Section in Reset or Foget Password Screen
+      - Logout Dialog
+
+    App Settings Module
+      - Settings Screen ( include app version )
+      - verify new version dialog
+      - More Screen
+      - About Us Screen
+      - Contact Us Screen
+      - Home Screen
+
+    Ads Module 
+      - Cars Search Result Screen
+      - Motorbike Search Result Screen
+      - Bike Search Result Screen
+      - Cars Details Screen
+      - Bike Details Screen
+      - Motorbike Details Screen
+      - MyAds Screen
+      - My ViewBooking Screen
+      - ViewBooking Update Status Dialog
+      - ViewBooking Submit Review Screen
+      - View Availability Section
+      - Wishlist Screen
+      - Add new Ad Screen
+      - Edit Ad Screen
+      - BloggerReviews Section
+      - BloggerReview Full Dialog
+
+    Notification Module
+      - My Notification Screen
+
+
+*/
